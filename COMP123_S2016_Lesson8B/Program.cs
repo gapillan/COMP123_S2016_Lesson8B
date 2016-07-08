c1bdf94d7676617f8fa2bba6031431a3d4118abd
gapillan <gapillan@my.centennialcollege.ca>
2016-07-08 10:58:55 -0400

    FileHandling

---
+++ COMP123_S2016_Lesson8B/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using COMP123_S2016_Lesson8B.Models;
+
+namespace COMP123_S2016_Lesson8B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+        }
+    }
+}
